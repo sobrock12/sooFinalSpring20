0c2b25b1f83434b9608fba6fec6867f62ee51049
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2020-05-06 22:41:02 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,10 +1,11 @@
 <?php
     
-    $dsn = 'mysql:host=localhost;dbname=api';
-    $username = 'root';
+    $dsn = 'mysql:host=ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=ftk923rzylpzqsk3';
+    $username = 'wune56svj1j0cb9s';
+    $pw = 'fb49yc6cjs9wj0uz';
 
         try {
-            $db = new PDO($dsn, $username);
+            $db = new PDO($dsn, $username, $pw);
         } catch (PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
         exit();
